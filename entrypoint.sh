50f8c62a13d062750424cdb65efbd7422b131c41
Sven Speckmaier <sps@ipunkt.biz>
2017-03-14 08:09:06 +0100

    moved rancher download to function

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,20 +8,30 @@ if [ ! -d ".rancherize" ] ; then
 	mkdir .rancherize
 fi
 
-if [ -z "${RANCHER_COMPOSE_VERSION}" ] ; then
-	RANCHER_COMPOSE_VERSION="0.12.2"
-fi
 
-RANCHER_COMPOSE="rancher-compose-${RANCHER_COMPOSE_VERSION}"
-TARGET_RANCHER_COMPOSE="/usr/local/bin/$RANCHER_COMPOSE"
-if [ ! -f "$TARGET_RANCHER_COMPOSE" ] ; then
-	echo "rancher-compose version $RANCHER_COMPOSE_VERSION not found. Downloading."
-	curl "https://releases.rancher.com/compose/v${RANCHER_COMPOSE_VERSION}/rancher-compose-linux-amd64-v${RANCHER_COMPOSE_VERSION}.tar.gz" \
-		| gunzip | tar -C /tmp -x
-	cp /tmp/rancher-compose-v${RANCHER_COMPOSE_VERSION}/rancher-compose "$TARGET_RANCHER_COMPOSE"
-	chmod +x "$TARGET_RANCHER_COMPOSE"
-	echo "Installed Rancher compose $RANCHER_COMPOSE_VERSION to $TARGET_RANCHER_COMPOSE"
-fi
+download_rancher_compose() {
+
+	RANCHER_COMPOSE_VERSION="$1"
+
+	if [ -z "${RANCHER_COMPOSE_VERSION}" ] ; then
+		RANCHER_COMPOSE_VERSION="0.12.2"
+	fi
+
+	RANCHER_COMPOSE="rancher-compose-${RANCHER_COMPOSE_VERSION}"
+	TARGET_RANCHER_COMPOSE="/usr/local/bin/$RANCHER_COMPOSE"
+	if [ ! -f "$TARGET_RANCHER_COMPOSE" ] ; then
+		echo "rancher-compose version $RANCHER_COMPOSE_VERSION not found. Downloading."
+		curl "https://releases.rancher.com/compose/v${RANCHER_COMPOSE_VERSION}/rancher-compose-linux-amd64-v${RANCHER_COMPOSE_VERSION}.tar.gz" \
+			| gunzip | tar -C /tmp -x
+		cp /tmp/rancher-compose-v${RANCHER_COMPOSE_VERSION}/rancher-compose "$TARGET_RANCHER_COMPOSE"
+		chmod +x "$TARGET_RANCHER_COMPOSE"
+		echo "Installed Rancher compose $RANCHER_COMPOSE_VERSION to $TARGET_RANCHER_COMPOSE"
+	fi
+
+	echo ${RANCHER_COMPOSE}
+}
+
+RANCHER_COMPOSE=`download_rancher_compose "$RANCHER_COMPOSE_VERSION"`
 
 sed \
 	-e "s~%%RANCHER_URL%%~${RANCHER_URL}~g" \
